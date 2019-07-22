e67a0e49c3f8c712cb4175b6ad9c932b83dd5fd2
Chloe Zhou <chloezhouny@gmail.com>
2019-07-21 23:32:27 -0700

    Add get all recipe, get single recipe, update recipe note

--- server.js
+++ server.js
@@ -71,6 +71,49 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+
+app.get("/recipes", function(req, res) {
+  // Grab every document in the Recipes collection
+  db.Recipe.find({})
+    .then(function(dbRecipe) {
+      res.json(dbRecipe);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+
+app.get("/recipes/:id", function(req, res) {
+  db.Recipe.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbRecipe) {
+      res.json(dbRecipe);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/recipes/:id", function(req, res) {
+
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Recipe to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Recipe.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbRecipe) {
+      res.json(dbRecipe);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+
+
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
